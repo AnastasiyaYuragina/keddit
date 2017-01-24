6017233911db4d27792c0e85ee130e020062949b
Anastasiya Yuragina <dark25555@gmail.com>
2017-01-24 16:17:00 +0200

    added new NewsFragment and Extensions file

--- app/src/main/java/com/anastasiyayuragina/keddit/NewsFragment.kt
+++ app/src/main/java/com/anastasiyayuragina/keddit/NewsFragment.kt
@@ -7,13 +7,17 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.anastasiyayuragina.keddit.commons.Extensions
+import com.anastasiyayuragina.keddit.commons.inflate
 
 class NewsFragment : Fragment() {
     private var newsListView: RecyclerView? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.news_fragment, container, false)
+
+        val view = container?.inflate(R.layout.news_fragment)
+
         newsListView.run {
             view.findViewById(R.id.newsList)
             this?.setHasFixedSize(true)
